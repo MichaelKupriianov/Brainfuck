71fd5d4943a56e173edc628c22e5be993fe84b3d
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-03-30 02:00:02 +0300

    execute initialisation

---
+++ execute.go
@@ -0,0 +1,10 @@
+package main
+
+func executeProgram(s string) {
+	instructions := tokenize(s)
+	array := initialArray()
+	pointer := 0
+	for pointer < len(instructions) {
+		instructions[pointer].execute(&array, &pointer, &instructions)
+	}
+}
